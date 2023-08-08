772be295ef829692d6ded17ab16af9f282d0ec0c
Bertold Van den Bergh <vandenbergh@bertold.org>
2023-08-08 10:00:40 +0200

    Add support for PUYA P25Q21H

--- spiflash/types.go
+++ spiflash/types.go
@@ -19,6 +19,7 @@ var devices = []flashDevice{
 	{deviceID: 0x0e4012, name: "Freemont FT25H02", opcodeChipErase: 0xC7, opcodeBlockErase: 0x20, blockSize: 4096, opcodePageErase: 0xD8, pageSize: 256, chipSize: 256 * 1024},
 	{deviceID: 0x0e4013, name: "Freemont FT25H04", opcodeChipErase: 0xC7, opcodeBlockErase: 0x20, blockSize: 4096, opcodePageErase: 0xD8, pageSize: 256, chipSize: 512 * 1024},
 	{deviceID: 0xa13111a1, name: "Fudan Microelectronics FM25F01", opcodeChipErase: 0xC7, opcodeBlockErase: 0x20, blockSize: 4096, opcodePageErase: 0xD8, pageSize: 256, chipSize: 128 * 1024},
+	{deviceID: 0x85401285, name: "PUYA P25Q21H", opcodeChipErase: 0x60, opcodeBlockErase: 0x20, blockSize: 4096, opcodePageErase: 0x81, pageSize: 256, chipSize: 256 * 1024},
 }
 
 func rightAlign(in uint32) (uint32, uint32) {
